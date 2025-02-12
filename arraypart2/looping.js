77662874fb06e4acb170a70e3c42360ba750954e
mbiplobmollah <biplobmollah16@gmail.com>
2025-02-13 00:48:30 +0600

    this is for practice

---
+++ arraypart2/looping.js
@@ -0,0 +1,15 @@
+/*
+1.for loop
+2.while loop
+3.do while
+4.for of --> array loop korar jonno
+5.for in --> object loop korar jonno
+
+*/
+
+
+const fruits = ['orange', 'apple', 'watermelon', 'grapes','pinaple']
+
+for (const fruit of fruits){
+    console.log(fruit)
+}
