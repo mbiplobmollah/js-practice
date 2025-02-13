e508e73e2087079d361f8383e072f1e4f96f013b
mbiplobmollah <biplobmollah16@gmail.com>
2025-02-13 16:50:56 +0600

    learn about function

---
+++ functions and objects/function.js
@@ -0,0 +1,21 @@
+const price = 54;
+
+// function declare
+
+function fanOffKor(){
+    console.log('bosa theke dara')
+    console.log('fan er kache jaiya dara')
+    console.log('click kor fan er switch')
+}
+
+fanOffKor()
+
+// function name(){} --> this is the structure of a functin declare
+
+function buyBiriyani(){
+    console.log('taka ne amar kas thaika')
+    console.log('taka niya dokan e ja')
+    console.log('biriya kin tarpor bashay ay')
+}
+
+buyBiriyani()
