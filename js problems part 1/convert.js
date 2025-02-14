4f635eaa5107e4f19c88688ad2f258d9a14d4c26
mbiplobmollah <biplobmollah16@gmail.com>
2025-02-14 16:07:31 +0600

    done 2 problems

--- js problems part 1/convert.js
+++ js problems part 1/convert.js
@@ -1,5 +1,7 @@
 // 12 inch 1 feet
 
+//problem1
+
 function inchToFeet(inch){
     const feet = inch/12;
     return feet;
@@ -19,3 +21,13 @@ console.log(result1.toFixed(2));
 
 const result2 = inchToFeet2(75);
 console.log(result2);
+
+
+function mileToKilometer(mile){
+    const kilo = mile * 1.60934;
+    return kilo;
+}
+
+const resultKilo = mileToKilometer
+(75);
+console.log(resultKilo.toFixed(4));
