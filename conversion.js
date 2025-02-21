2aed10ac6d55f8fcec1a29e8f93ac56b62b1c9ff
mbiplobmollah <biplobmollah16@gmail.com>
2025-02-21 23:36:49 +0600

    know about conversion

---
+++ conversion.js
@@ -0,0 +1,9 @@
+let num = '10';
+console.log(num);
+console.log(Number(num));
+let str = 35;
+console.log(str);
+console.log(String(str));
+let boolean = 'true';
+console.log(boolean);
+console.log(Boolean(boolean));
